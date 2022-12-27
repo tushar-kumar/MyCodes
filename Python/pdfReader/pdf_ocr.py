d3b1be640c9ef186c47eec6a2a94422426a0ed54
Tushar Kumar <atul201kumar@gmail.com>
2022-12-28 02:11:19 +0530

    Update

--- Python/pdfReader/pdf_ocr.py
+++ Python/pdfReader/pdf_ocr.py
@@ -1,7 +1,8 @@
 from PIL import Image
 import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\KIYO\AppData\Local\Tesseract-OCR\tesseract.exe'
 
-file = Image.open("/home/user/sample.png")
+file = Image.open("sample.png")
 str = pytesseract.image_to_string(file, lang='eng')
 
 print(str)
