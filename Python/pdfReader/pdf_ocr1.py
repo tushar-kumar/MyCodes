2214e34dfbe2a7ada89a67d9937201107d523c94
Tushar Kumar <atul201kumar@gmail.com>
2022-12-28 14:09:35 +0530

    Update

---
+++ Python/pdfReader/pdf_ocr1.py
@@ -0,0 +1,22 @@
+import io
+from PIL import Image
+import pytesseract
+from wand.image import Image as wi
+
+pdfFile = wi(filename = ""/home/user/sample.pdf"", resolution = 300)
+image = pdfFile.convert('jpeg')
+
+imageBlobs = []
+
+for img in image.sequence:
+	imgPage = wi(image = img)
+	imageBlobs.append(imgPage.make_blob('jpeg'))
+
+extract = []
+
+for imgBlob in imageBlobs:
+	image = Image.open(io.BytesIO(imgBlob))
+	text = pytesseract.image_to_string(image, lang = 'eng')
+	extract.append(text)
+
+print(extract)
