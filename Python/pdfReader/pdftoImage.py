e6be067bd64dc91712fd4c0cc45fa9795d9331e5
Tushar Kumar <atul201kumar@gmail.com>
2022-12-29 18:03:56 +0530

    update

--- Python/pdfReader/pdftoImage.py
+++ Python/pdfReader/pdftoImage.py
@@ -3,5 +3,4 @@ doc = fitz.open('sample.pdf')
 for page in doc:
     pix = page.get_pixmap(matrix=fitz.Identity, dpi=100,
                           colorspace=fitz.csGRAY, clip=None, alpha=True, annots=True)
-    type(pix)
-    # pix.save("samplepdfimage-%i.jpg" % page.number)
+    pix.save("samplepdfimage-%i.jpg" % page.number)
